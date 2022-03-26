2aaa334335b5fdf1747122a1704f65121466bd69
tumuji <thosokawa56@gmail.com>
2022-03-26 11:31:49 +0900

    ホワイトリストチェックの修正（あいまい検索対応）

--- chkObservableUtils.js
+++ chkObservableUtils.js
@@ -3,28 +3,36 @@ var chkObservableUtils = Class.create();
 chkObservableUtils.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
 
     type: 'chkObservableUtils',
-	
-	chkWhitelist: function(){
-		var inc_id = this.getParameter('sysparm_incid');
-		var gr = new GlideRecord('x_yyyy_observable');
-		gr.addQuery('parent_incident', inc_id);
-		gr.query();
-		
-		while(gr.next()){
-			var obs_value = gr.getValue('value');
-			
-			var grWL = new GlideRecord('x_yyyy_whitelist');
-			grWL.addQuery('value', 'CONTAINS', obs_value);
-			grWL.query();
-			
-			if(grWL.next()){
-				gr.setValue('whitelist', '有り');
-				gr.update();
-			} else{
-				gr.setValue('whitelist', '無し');
-				gr.update();
-			}
-		}
-		return 0;
-	}
+
+    chkWhitelist: function() {
+        var inc_id = this.getParameter('sysparm_incid');
+        var gr = new GlideRecord('x_yyyy_observable');
+        gr.addQuery('parent_incident', inc_id);
+        gr.query();
+
+        while (gr.next()) {
+            var obs_value = gr.getValue('value');
+            var querystr = "valueLIKE" + obs_value;
+            var obs_type = gr.getValue('observable_type');
+
+            var grWL = new GlideRecord('x_yyyy_whitelist');
+            //ファイル名又はコマンドのときはあいまい検索。それ以外は完全一致
+            if ((obs_type == "commandline") || (obs_type == "filename")) {
+                gr.addEncodedQuery(querystr);
+            } else {
+                grWL.addQuery('value', '=', obs_value);
+            }
+
+            grWL.query();
+
+            if (grWL.next()) {
+                gr.setValue('whitelist', '有り');
+                gr.update();
+            } else {
+                gr.setValue('whitelist', '無し');
+                gr.update();
+            }
+        }
+        return 0;
+    }
 });
