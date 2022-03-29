226642473050436f3bedd3565ccbb4913091cf4c
tumuji <thosokawa56@gmail.com>
2022-03-29 19:26:21 +0900

    あいまい検索のバグを修正

--- chkObservableUtils.js
+++ chkObservableUtils.js
@@ -5,32 +5,51 @@ chkObservableUtils.prototype = Object.extendsObject(global.AbstractAjaxProcessor
     type: 'chkObservableUtils',
 
     chkWhitelist: function() {
+
         var inc_id = this.getParameter('sysparm_incid');
-        var gr = new GlideRecord('x_yyyy_observable');
-        gr.addQuery('parent_incident', inc_id);
-        gr.query();
 
-        while (gr.next()) {
-            var obs_value = gr.getValue('value');
-            var querystr = "valueLIKE" + obs_value;
-            var obs_type = gr.getValue('observable_type');
+        //デフォルト値をホワイトリスト該当「無し」にする。
+        var grDF = new GlideRecord('x_yyyy_observable');
+        grDF.query();
 
-            var grWL = new GlideRecord('x_yyyy_whitelist');
+        while (grDF.next()) {
+            grDF.setValue('whitelist', '無し');
+            grDF.update();
+        }
+
+        //
+		//観測事象の各値がホワイトリストの中に存在するか検索するのが自然だが、ここでは逆にホワイトリストの各値について、
+		//当該インシデントの観測事象の中に存在するか検索する。これはファイル名とコマンドのあいまい検索を実現するためである。
+        //具体的にいうと、観測事象の値が「C¥USER¥white.exe」で、ホワイトリストが「white.exe」の場合は、観測事象の値がホワイトリストの値を
+		//CONTAINS(含む)という条件になる。クエリにはCONTAINSの逆の演算子はない。
+
+        var grWL = new GlideRecord('x_yyyy_whitelist');
+        grWL.query();
+
+        while (grWL.next()) {
+            var wl_value = grWL.getValue('value');
+
             //ファイル名又はコマンドのときはあいまい検索。それ以外は完全一致
-            if ((obs_type == "commandline") || (obs_type == "filename")) {
-                gr.addEncodedQuery(querystr);
-            } else {
-                grWL.addQuery('value', '=', obs_value);
+            var grFC = new GlideRecord('x_yyyy_observable');
+            grFC.addQuery('parent_incident', inc_id);
+            grFC.addQuery('observable_type', 'IN', 'filename, commandline');
+            grFC.addQuery('value', 'CONTAINS', wl_value);
+            grFC.query();
+
+            while (grFC.next()) {
+                grFC.setValue('whitelist', '有り');
+                grFC.update();
             }
+            //ファイル名又はコマンド以外は完全一致
+            var grOT = new GlideRecord('x_yyyy_observable');
+            grOT.addQuery('parent_incident', inc_id);
+            grOT.addQuery('observable_type', 'NOT IN', 'filename, commandline');
+            grOT.addQuery('value', '=', wl_value);
+            grOT.query();
 
-            grWL.query();
-
-            if (grWL.next()) {
-                gr.setValue('whitelist', '有り');
-                gr.update();
-            } else {
-                gr.setValue('whitelist', '無し');
-                gr.update();
+            while (grOT.next()) {
+                grOT.setValue('whitelist', '有り');
+                grOT.update();
             }
         }
         return 0;
